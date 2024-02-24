64722ccb9c02c557b2d7650a2510bb576a71c50c
TerraBean <mujahidadams@ymail.com>
2024-02-24 09:22:33 +0000

    add pagination

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,4 +1,37 @@
+'use client'
 
-export default function Page(){
-    return <p>Customers page</p>
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
+
+
+useSearchParams
+export default function Page({searchParams}:{
+    searchParams?:{
+        query?:string;
+        current?:string;
+    }
+}){
+
+    const searchParameters = useSearchParams()
+    const pathName = usePathname()
+    const {replace}= useRouter()
+
+    function handleChange(term:string){
+        const params = new URLSearchParams(searchParameters)
+        if(term){
+            params.set('query',term)
+            params.set('current','customer')
+        }else{
+            params.delete('query')
+            params.delete('current')
+        }
+        replace(`${pathName}?${params.toString()}`)
+    }
+    return (
+        <>
+            <input type="text" onChange={(e)=>{
+                handleChange(e.target.value)
+            }} />
+        </>
+    )
 }
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,8 +9,10 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
+import { unstable_noStore as noStore } from 'next/cache';
 
 export async function fetchRevenue() {
+  noStore()
   // Add noStore() here to prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
 
@@ -18,12 +20,14 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
  
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.');
+    console.log(data.rows[0].month);
+    
 
     return data.rows;
   } catch (error) {
@@ -33,6 +37,7 @@ export async function fetchRevenue() {
 }
 
 export async function fetchLatestInvoices() {
+  noStore()
   try {
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
@@ -40,6 +45,9 @@ export async function fetchLatestInvoices() {
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
       LIMIT 5`;
+
+      // console.log(data.rows[0]);
+      
 
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
@@ -53,6 +61,7 @@ export async function fetchLatestInvoices() {
 }
 
 export async function fetchCardData() {
+  noStore()
   try {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
@@ -69,7 +78,10 @@ export async function fetchCardData() {
       customerCountPromise,
       invoiceStatusPromise,
     ]);
+    // console.log(data[0].rows[0])
+    
 
+    // Return 0 if data.rows.count is undefined or null and convert it to a number
     const numberOfInvoices = Number(data[0].rows[0].count ?? '0');
     const numberOfCustomers = Number(data[1].rows[0].count ?? '0');
     const totalPaidInvoices = formatCurrency(data[2].rows[0].paid ?? '0');
@@ -89,11 +101,14 @@ export async function fetchCardData() {
 
 const ITEMS_PER_PAGE = 6;
 export async function fetchFilteredInvoices(
+  
   query: string,
   currentPage: number,
 ) {
+  noStore()
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
+  
   try {
     const invoices = await sql<InvoicesTable>`
       SELECT
@@ -124,6 +139,7 @@ export async function fetchFilteredInvoices(
 }
 
 export async function fetchInvoicesPages(query: string) {
+  noStore()
   try {
     const count = await sql`SELECT COUNT(*)
     FROM invoices
@@ -137,6 +153,7 @@ export async function fetchInvoicesPages(query: string) {
   `;
 
     const totalPages = Math.ceil(Number(count.rows[0].count) / ITEMS_PER_PAGE);
+    
     return totalPages;
   } catch (error) {
     console.error('Database Error:', error);
@@ -145,6 +162,7 @@ export async function fetchInvoicesPages(query: string) {
 }
 
 export async function fetchInvoiceById(id: string) {
+  noStore()
   try {
     const data = await sql<InvoiceForm>`
       SELECT
@@ -170,6 +188,7 @@ export async function fetchInvoiceById(id: string) {
 }
 
 export async function fetchCustomers() {
+  noStore()
   try {
     const data = await sql<CustomerField>`
       SELECT
@@ -188,6 +207,7 @@ export async function fetchCustomers() {
 }
 
 export async function fetchFilteredCustomers(query: string) {
+  noStore()
   try {
     const data = await sql<CustomersTableType>`
 		SELECT
@@ -221,6 +241,7 @@ export async function fetchFilteredCustomers(query: string) {
 }
 
 export async function getUser(email: string) {
+  noStore()
   try {
     const user = await sql`SELECT * FROM users WHERE email=${email}`;
     return user.rows[0] as User;
